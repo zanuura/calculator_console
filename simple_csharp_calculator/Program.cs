dd9146e91c90184f5a66c257bf02cd71ab13bb44
Hammam Hanan <73764446+zanuura@users.noreply.github.com>
2023-09-28 14:18:57 +0700

    first commit

---
+++ simple_csharp_calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SIMPLE CALCULATOR");
+            Console.WriteLine("\n");
+
+            //Console.Write("Enter number: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter another: ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadLine();
+        }
+    }
+}
